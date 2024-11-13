6ddcde5176bdb28c9d9686e734b018118c43f244
Thiago Lopes <tiagojosecnn@outlook.com>
2024-11-12 23:45:45 -0300

    adicona enpoint de atualizacao ao routes.

--- restaurante-app-backend/src/routes/productRoutes.ts
+++ restaurante-app-backend/src/routes/productRoutes.ts
@@ -1,9 +1,10 @@
-import { product, products, productFindOne } from "../api/controllers/productController"; 
+import { product, products, productFindOne, updateProduct } from "../api/controllers/productController"; 
 
 const router = require("express").Router();
 
 router.post('/products', product); //rota para endpoint de cadastro de usuários
 router.get('/products', products); //rota para retornar todos os produtos
 router.get('/products/:id', productFindOne); //rota para uscar um produto
+router.put('/products/:id', updateProduct); //rota para atualizar dados de um produto
 
 export default router;
